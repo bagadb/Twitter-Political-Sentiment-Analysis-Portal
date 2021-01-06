f4f7510b0460b8027ddb92f4b34900ef91dff51d
Shreyas Lolage <shreyaslolage@gmail.com>
2021-01-06 11:20:29 +0530

    Added Angular

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const favicon = require("serve-favicon"); 
 const dealer = express();
+const bodyparser = require("body-parser");
+
 const port = 8080
 const root = { root: __dirname }
 const path = require('path')
@@ -8,11 +10,20 @@ const path = require('path')
 console.log("Starting Backend Server!!");
 
 dealer.use(favicon(path.join(__dirname, 'favicon.ico'), 10))
+dealer.use(bodyparser.json())
+
+dealer.get('/style.css', (req, res) => {
+  res.sendFile('./style.css', root )
+})
 
 dealer.get('/', (req, res) => {
-    res.sendFile('./index.html', root )
-  })
+  res.sendFile('./index.html', root )
+})
+
+dealer.post('/', (req,res) => {
+    console.log(req.body)
+})
 
 dealer.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`API Started at http://localhost:${port}`)
 })
