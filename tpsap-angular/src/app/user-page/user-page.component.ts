0b50b97be6fdfc9b989f7c7f535772ddf0a02e93
bagadb <shreyaslolage@gmail.com>
2021-05-12 15:37:19 +0530

    UI Changes

--- tpsap-angular/src/app/user-page/user-page.component.ts
+++ tpsap-angular/src/app/user-page/user-page.component.ts
@@ -8,15 +8,25 @@ import { AdminpanelComponent } from '../adminpanel/adminpanel.component'
   styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent implements OnInit {
-
+  
   AdminLogin = false;
-  loggedInUser: String
+  loggedInUser: String;
+
+  listOfTabs = [ "Query Panel" , "Query History" ];
+
+
+  selectedTab = 0;
+
+  switchTab(tabNumber){
+    this.selectedTab = tabNumber;
+  }
 
   constructor(private route: ActivatedRoute) {
     this.route.params.subscribe(params => {
       this.loggedInUser = params['username'];
       if (this.loggedInUser == "admin"){
         this.AdminLogin = true;
+        this.listOfTabs = [ "Query Panel" , "Query History" , "Admin Panel" ];
       }
     });
    }
